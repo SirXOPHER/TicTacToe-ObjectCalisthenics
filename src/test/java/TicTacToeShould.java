35a18c6359435bcb4f6141b24507c907ca5448f7
SirXOPHER <chris.reinmuller@gmail.com>
2018-07-05 14:49:03 +0100

    Added first test 'ensurePlayerXalwaysGoesFirst()'

---
+++ src/test/java/TicTacToeShould.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Created by xopher on 05/07/2018.
+ */
+public class TicTacToeShould {
+
+    @Test
+    public void ensurePlayerXalwaysGoesFirst() {
+        assertThat(new TicTacToe().makeMark, is("X"));
+    }
+}
